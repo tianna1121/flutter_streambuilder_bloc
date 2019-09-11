289d3d389168f96404d18762e96dd4f2bac55395
yang zhang <tianna1121@gmail.com>
2019-09-11 15:46:05 +0800

    Initial commit. Try use Bloc and Streambuilder to build apps. based on: https://www.youtube.com/watch?v=PRd4Y_E2od4

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      title: 'StreamBuilder & BLOC',
+      home: MyHomePage(),
+    ));
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
